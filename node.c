21fb2b2cef08523d1c351686df42bebbbadbc723
diogoegrodrigues <diogoegrodrigues@gmail.com>
2019-10-05 18:28:44 +0100

    delete beginning

--- node.c
+++ node.c
@@ -22,6 +22,49 @@ struct node* newNode(size_t nextHop)
 	return(node); 
 }
 
+/*******************************************************************************/
+
+struct node* PrefixTree(char* filename)
+{
+	FILE *fp = fopen( filename, "r" );
+	if( fp == NULL )
+	{
+		perror( filename );
+		exit(1);
+	}
+
+	struct node *root;
+
+	char line[128]; /* maximum line size */
+
+	while( fgets( line, sizeof line, fp ) != NULL ) // read a line
+	{
+		// Returns first token
+		char *token = strtok(line, " ");
+		char *token2 = strtok(NULL, " ");
+
+		printf("token: %s\n", token);
+		printf("token2: %s\n", token2);
+
+		if( ( token == NULL ) && ( token2 == NULL ) )
+		{
+			printf("Input file in the wrong structure");
+			exit(1);
+		}
+
+		if(token[0] == 'e')
+			root = InsertPrefix(NULL, token, atoi(token2));
+		else
+			root = InsertPrefix(root, token, atoi(token2));
+
+		printf("next line\n\n");
+	}
+
+	fclose( fp );
+
+	return root;
+}
+
 void InsertPrefix(struct node * root, char [] prefix, size_t nextHop){
 	if (root == NULL){
 		root = newNode(nextHop);
@@ -81,3 +124,21 @@ void PrintTable (struct node * root){
 	return;
 }
 
+struct node* DeletePrefix(struct node* root, char * prefix)
+{
+	struct node* aux;
+
+	if( root == NULL )
+	{
+		print("Empty tree\n");
+		return;
+	}
+
+	for(int i = 0; i = strlen(prefix); i++)
+	{
+		if(prefix[i] == '0')
+		{
+			
+		}
+	}
+}
