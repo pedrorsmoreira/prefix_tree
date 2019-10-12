4f0e39a171196b71031f22c80ca23d8d83047d4f
diogoegrodrigues <diogoegrodrigues@gmail.com>
2019-10-12 19:41:41 +0100

    9

--- node.h
+++ node.h
@@ -2,16 +2,9 @@
 #ifndef NODE
 #define NODE
 
-// Structures
-struct node  
-{ 
-		size_t nextHop;
-		struct node *left; 
-		struct node *right;
-		struct intList * hopsList;
-};
+typedef struct node Node;
 
-struct node * newNode (size_t nextHop);
+Node* newNode (size_t nextHop);
 
 /* End if: Protect multiple inclusions                              */
 #endif
