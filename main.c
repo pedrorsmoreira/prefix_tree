438de1a23be3531c6d3e46546b85aecbb6bcb862
irmaoshiva <rsmoreira.pedro@gmail.com>
2019-10-13 00:25:53 +0100

    w

--- main.c
+++ main.c
@@ -35,10 +35,54 @@ int main(int argc, char *argv[])
 	Node* root = PrefixTree (filename);
 	PrintTable(root);
 
-	printf("Prefix %s Next-hop: %d\n", "11010011", LookUp (root, "11010011"));
-	printf("Prefix %s Next-hop: %d\n", "11000011", LookUp (root, "11000011"));
+	while(1){
+		//read the action from stdin and perform it
+		while (1){
+		char order, aux[50], prefix[16], nexthop[16];
+			printf("(Enter [Q] to quit) \nIntroduce the action, Print [P], Search [S], Insert [I], Delete [D] or Compress [C]:");
+			if (fgets(aux, sizeof(aux), stdin) == NULL){
+				perror("fgets: ");
+				exit(-1);
+			}
+			sscanf(aux, "%c", &order);
+			//perform
+			if ((order == 'q') || (order == 'Q'))
+				return 0;
 
-	printf("\n");
+			else if ((order == 'i') || (order == 'I')){
+				printf("Write the prefix to be inserted:");	
+				if (fgets(prefix, 16, stdin) == NULL){
+					perror("fgets: ");
+					exit(-1);
+				}
+				printf("Write the nexthop:");	
+				if (fgets(nexthop, 16, stdin) == NULL){
+					perror("fgets: ");
+					exit(-1);
+				}
+				Node* r = InsertPrefix(root, prefix, atoi(nexthop));
+				if (r != NULL) root = r;
+				else printf("invalid input.\n---\n\n");
+
+				break;
+			}
+			else if ((order == 'P') || (order == 'p')){
+				PrintTable(root);
+				break;
+			}
+			else if ((order == 'W') || (order == 'w')){
+				//read the region of the action from stdin
+				region = read_region();
+				
+				nbytes = clipboard_wait(fd, region, message, MESSAGE_SIZE);
+				if (nbytes == 0)	printf("paste (wait) failed\n---\n");
+				else	printf("Received %s\n (%d bytes pasted (wait))\n---\n", message, nbytes);
+				
+				break;
+			}
+			printf("Option not available\n---\n");
+		}
+	}
 
 	//root = CompressTree (root);
 
